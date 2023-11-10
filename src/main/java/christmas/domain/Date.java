7676747906f773fcfc26ddb1f270a9a6d8dfcced
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-11-11 02:25:45 +0900

    refactor(Date): 매직 넘버 및 상수 정리

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -1,6 +1,9 @@
 package christmas.domain;
 
 public class Date {
+    private static final int MINIMUM_DATE = 1;
+    private static final int MAXIMUM_DATE = 31;
+    private static final String INVALID_DATE_MESSAGE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     private final int date;
 
     public Date(int date) {
@@ -13,8 +16,12 @@ public class Date {
     }
 
     private void validateRange(int date) {
-        if (date < 1 || date > 31) {
-            throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        if (date < MINIMUM_DATE || date > MAXIMUM_DATE) {
+            throw new IllegalArgumentException(INVALID_DATE_MESSAGE);
         }
+    }
+
+    public int getDate() {
+        return date;
     }
 }
