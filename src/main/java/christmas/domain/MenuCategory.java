55a9b35d268ee06beb2bed8743998335216fb1bd
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-11-11 15:59:17 +0900

    feat(MenuCategory): 메뉴 카테고리 클래스 추가

---
+++ src/main/java/christmas/domain/MenuCategory.java
@@ -0,0 +1,19 @@
+package christmas.domain;
+
+public enum MenuCategory {
+    APPETIZER("애피타이저"),
+    MAIN("메인"),
+    DESSERT("디저트"),
+    DRINK("음료");
+
+    private final String category;
+
+    MenuCategory(String description) {
+        this.category = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
+
