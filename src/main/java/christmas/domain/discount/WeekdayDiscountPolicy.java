6079ebce557fcd8d25929f43b9bc2d3effb29324
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-11-11 22:21:35 +0900

    feat(WeekdayDiscountPolicy): 평일 할인 정책 기능 구현

---
+++ src/main/java/christmas/domain/discount/WeekdayDiscountPolicy.java
@@ -0,0 +1,73 @@
+package christmas.domain.discount;
+
+import christmas.constant.DiscountConstants;
+import christmas.domain.Menu;
+import christmas.domain.MenuCategory;
+import christmas.domain.order.Order;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Map;
+
+public class WeekdayDiscountPolicy implements DiscountPolicy {
+
+    private static final int WEEKDAY_DISCOUNT_AMOUNT = 2023;
+
+    @Override
+    public int discount(Order order) {
+        LocalDate orderDate = order.getDate();
+
+        if (isWithinDiscountPeriod(orderDate) && isWeekday(orderDate)) {
+            return calculateTotalDiscount(order);
+        }
+        return DiscountConstants.NO_DISCOUNT;
+    }
+
+    private int calculateTotalDiscount(Order order) {
+        int totalDiscount = 0;
+        Map<Menu, Integer> menus = order.getMenus();
+
+        for (Map.Entry<Menu, Integer> menu : menus.entrySet()) {
+            totalDiscount += calculateDiscountForMenu(menu);
+        }
+        return totalDiscount;
+    }
+
+    private int calculateDiscountForMenu(Map.Entry<Menu, Integer> menu) {
+        if (isDessertCategory(menu.getKey())) {
+            return calculateMenuDiscount(menu);
+        }
+        return 0;
+    }
+
+    private boolean isDessertCategory(Menu menu) {
+        return menu.getMenuCategory() == MenuCategory.DESSERT;
+    }
+
+    private int calculateMenuDiscount(Map.Entry<Menu, Integer> menu) {
+        return WEEKDAY_DISCOUNT_AMOUNT * menu.getValue();
+    }
+
+    private boolean isWithinDiscountPeriod(LocalDate date) {
+        return isNotBeforeStartDate(date) && isNotAfterEndDate(date);
+    }
+
+    private boolean isNotBeforeStartDate(LocalDate date) {
+        return !date.isBefore(DiscountConstants.START_DAY_OF_MONTH);
+    }
+
+    private boolean isNotAfterEndDate(LocalDate date) {
+        return !date.isAfter(DiscountConstants.END_DAY_OF_MONTH);
+    }
+
+    private boolean isWeekday(LocalDate date) {
+        return isNotFriday(date) && isNotSaturday(date);
+    }
+
+    private boolean isNotFriday(LocalDate date) {
+        return date.getDayOfWeek() != DayOfWeek.FRIDAY;
+    }
+
+    private boolean isNotSaturday(LocalDate date) {
+        return date.getDayOfWeek() != DayOfWeek.SATURDAY;
+    }
+}
---
+++ src/test/java/christmas/domain/discount/WeekdayDiscountPolicyTest.java
@@ -0,0 +1,26 @@
+package christmas.domain.discount;
+
+import christmas.domain.Menu;
+import christmas.domain.order.Date;
+import christmas.domain.order.Order;
+import christmas.util.EventDateUtil;
+import java.util.EnumMap;
+import java.util.Map;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WeekdayDiscountPolicyTest {
+
+    @DisplayName("평일에 디저트 1개당 2023원을 할인 한다.")
+    @Test
+    void christmasDailyDiscount() {
+        Map<Menu, Integer> orderMenus = new EnumMap<>(Menu.class);
+        orderMenus.put(Menu.findByName(Menu.CHOCOLATE_CAKE.getName()), 4);
+        Order order = new Order(EventDateUtil.getLocalDateFromDay(new Date(18).getDate()), orderMenus);
+        int discount = new WeekdayDiscountPolicy().discount(order);
+
+        Assertions.assertThat(discount).isEqualTo(2023 * 4);
+    }
+
+}
