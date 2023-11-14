f85cca549f12dadbcd71b996a78105f887a3107c
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-11-14 11:32:27 +0900

    test(Badge): ParameterizedTest로 변경

--- src/test/java/christmas/domain/BadgeTest.java
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -3,17 +3,15 @@ package christmas.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BadgeTest {
 
     @DisplayName("가격에 따른 뱃지를 반환 한다.")
-    @Test
-    void getBadgeByDiscountPriceTest() {
-        assertThat(Badge.getBadgeByDiscountPrice(0)).isEqualTo(Badge.NONE);
-        assertThat(Badge.getBadgeByDiscountPrice(5000)).isEqualTo(Badge.STAR);
-        assertThat(Badge.getBadgeByDiscountPrice(10000)).isEqualTo(Badge.TREE);
-        assertThat(Badge.getBadgeByDiscountPrice(20000)).isEqualTo(Badge.SANTA);
-        assertThat(Badge.getBadgeByDiscountPrice(25000)).isEqualTo(Badge.SANTA);
+    @ParameterizedTest
+    @CsvSource({"0, NONE", "5000, STAR", "10000, TREE", "20000, SANTA", "25000, SANTA"})
+    void getBadgeByDiscountPriceTest(int discountPrice, Badge expectedBadge) {
+        assertThat(Badge.getBadgeByDiscountPrice(discountPrice)).isEqualTo(expectedBadge);
     }
 }
